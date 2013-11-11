507475c276bb71b521d79f64cfa664e6b6c089c2
Corey Leigh Latislaw <colabug@gmail.com>
2013-11-11 14:19:35 -0800

    Step 6: Add click listener

--- src/com/colabug/TardisLab/HelloTardisActivity.java
+++ src/com/colabug/TardisLab/HelloTardisActivity.java
@@ -1,14 +1,30 @@
 package com.colabug.TardisLab;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 public class HelloTardisActivity extends FragmentActivity
 {
     @Override
-    public void onCreate( Bundle savedInstanceState )
+    public void onCreate (Bundle savedInstanceState)
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.hello_tardis );
+
+        Fragment fragment = getSupportFragmentManager().findFragmentById( R.id.outside_fragment );
+        fragment.getView().setOnClickListener( generateOutsideClickListener() );
+    }
+
+    private View.OnClickListener generateOutsideClickListener ()
+    {
+        return new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View view)
+            {
+            }
+        };
     }
 }
