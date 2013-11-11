6e879732adab6d4f75a5ed466a73698c3ab96e32
Corey Leigh Latislaw <colabug@gmail.com>
2013-11-11 14:58:39 -0800

    Step 8: Behave properly

--- src/com/colabug/TardisLab/HelloTardisActivity.java
+++ src/com/colabug/TardisLab/HelloTardisActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class HelloTardisActivity extends FragmentActivity
 {
@@ -22,9 +23,29 @@ public class HelloTardisActivity extends FragmentActivity
         return new View.OnClickListener()
         {
             @Override
-            public void onClick (View view)
+            public void onClick( View view )
             {
+                // "Tablet"
+                if ( isMultiPaneLayout() )
+                {
+                    Toast.makeText( HelloTardisActivity.this,
+                                    "PARADOX",
+                                    Toast.LENGTH_SHORT ).show();
+                }
+                // "Phone"
+                else
+                {
+                    startActivity( InsideTardisActivity.createIntent( HelloTardisActivity.this ) );
+                }
             }
         };
+    }
+
+    private boolean isMultiPaneLayout()
+    {
+        Fragment insideFragment = getSupportFragmentManager().findFragmentById( R.id.inside_fragment );
+        return insideFragment != null &&
+               insideFragment.isInLayout() &&
+               insideFragment.isVisible();
     }
 }
